1fb4729e380e816cfdb032c7daadd935055fe430
DeceptiveRat <0101087ssk@gmail.com>
2025-10-11 17:04:12 +0900

    second commit, most bugs fixed

--- player.c
+++ player.c
@@ -4,25 +4,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
 
 #define PLAYER 1
 #define OPPONENT -1
 
 void printHelp(const char *argv);
-int getPossibleOutcomes(int8_t current_state[], int8_t* R_tensor[][27][27]);
+int getPossibleOutcomes(int8_t current_state[], int8_t R_tensor[][27][27][9]);
 bool isGameover(const int8_t current_state[], int8_t player);
-int8_t* getRMatrix(const int8_t current_state[], int8_t* R_tensor[][27][27]);
+int8_t* getRMatrix(const int8_t current_state[], int8_t R_tensor[][27][27][9]);
 int getHash(const int8_t current_state[]);
-bool setRMatrix(const int8_t current_state[], int8_t* R_tensor[][27][27], int8_t R_matrix[]);
+int unhash(const int* hash, int8_t return_value[]);
+bool setRMatrix(const int8_t current_state[], int8_t R_tensor[][27][27][9], int8_t R_matrix[]);
 
 int main(int argc, char **argv)
 {
 	int opt;
 	int mode = 0;
-	int8_t *R_tensor[27][27][27];
-	int8_t *Q_tensor[27][27][27];
-	memset(R_tensor, 0, 27*27*27);
-	memset(Q_tensor, 0, 27*27*27);
+	int8_t R_tensor[27][27][27][9];
+	int8_t Q_tensor[27][27][27][9];
+	memset(R_tensor, 0, 27*27*27*9);
+	memset(Q_tensor, 0, 27*27*27*9);
+
+	int8_t test[9] = {0, -1,-1, 0,-1, -1, -1, -1, -1};
+	int8_t result[9];
+	int hash = getHash(test);
+	unhash(&hash, result);
+	printf("%d\n", memcmp(test, result, 9));
 
 	while((opt = getopt(argc, argv, ":ht")) != -1)
 	{
@@ -43,19 +51,31 @@ int main(int argc, char **argv)
 	else if(mode == 1)
 	{
 		int8_t current_state[9] = {0};
-		getPossibleOutcomes(current_state, R_tensor);
+		if(getPossibleOutcomes(current_state, R_tensor) == -1)
+		{
+			printf("error getting outcomes\n");
+			exit(-1);
+		}
+
 		for(int i = 0;i<27;i++)
 		{
 			for(int j = 0;j<27;j++)
 			{
 				for(int k =0;k<27;k++)
 				{
-					if(R_tensor[i][j][k] != NULL)
+					int8_t empty[9] = {0};
+					if(memcmp(empty, R_tensor[i][j][k], 9) != 0)
 					{
 						printf("==============================================================\n");
+						printf("i: %2d, j: %2d, k: %2d\n", i, j, k);
 						int8_t temp[9];
+						int hash = (i<<24)|(j<<16)|(k<<8)|0x00;
+						printf("board:\n");
+						unhash(&hash, temp);
+						printf("%-3d %8d %8d\n%-3d %8d %8d\n%-3d %8d %8d\n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]);
+						printf("R matrix:\n");
 						memcpy(temp, R_tensor[i][j][k], 9);
-						printf("%d %d %d\n%d %d %d\n%d %d %d\n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]);
+						printf("%-3d %8d %8d\n%-3d %8d %8d\n%-3d %8d %8d\n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]);
 					}
 				}
 			}
@@ -73,7 +93,7 @@ void printHelp(const char *argv)
 	printf("-t: to train model\n");
 }
 
-int getPossibleOutcomes(int8_t current_state[], int8_t* R_tensor[][27][27])
+int getPossibleOutcomes(int8_t current_state[], int8_t R_tensor[][27][27][9])
 {
 	int8_t R_matrix[9];
 	memcpy(R_matrix, current_state, 9);
@@ -83,7 +103,7 @@ int getPossibleOutcomes(int8_t current_state[], int8_t* R_tensor[][27][27])
 			continue;
 		else
 		{
-			int8_t reward = 0;
+			int reward = 0;
 			current_state[i] = PLAYER;
 			if(isGameover(current_state, PLAYER))
 				reward = 100;
@@ -111,7 +131,10 @@ int getPossibleOutcomes(int8_t current_state[], int8_t* R_tensor[][27][27])
 
 			current_state[i] = 0;
 			R_matrix[i] = reward;
-			setRMatrix(current_state, R_tensor, R_matrix);
+			if(!setRMatrix(current_state, R_tensor, R_matrix))
+				return -1;
+
+			R_matrix[i] = 0;
 		}
 	}
 
@@ -137,10 +160,10 @@ bool isGameover(const int8_t current_state[], int8_t player)
 	return false;
 }
 
-int8_t* getRMatrix(const int8_t current_state[], int8_t* R_tensor[][27][27])
+int8_t* getRMatrix(const int8_t current_state[], int8_t R_tensor[][27][27][9])
 {
 	int hash = getHash(current_state);
-	if((hash&0xff) == 0xff)
+	if((hash&0xff000000) != 0x00)
 		return NULL;
 	return R_tensor[hash>>24][hash>>16][hash>>8];
 }
@@ -153,15 +176,8 @@ int getHash(const int8_t current_state[])
 	{
 		for(int j=0;j<3;j++)
 		{
-			int8_t byte = (*current_state)>>(8*(3*i+j));
-			if(byte == 0xff)
-				byte = 0;
-			else if(byte == 0x01)
-				byte = 1;
-			else if (byte == 0x00)
-				byte = 2;
-			else
-				return 0xffffffff;
+			int8_t byte = current_state[3*i+j];
+			byte+=1;
 
 			if(j==0)
 				indices[i] += 3*3*byte;
@@ -172,14 +188,37 @@ int getHash(const int8_t current_state[])
 		}
 	}
 
-	return (int)(*indices);
+	return *(int*)(indices);
 }
 
-bool setRMatrix(const int8_t current_state[], int8_t* R_tensor[][27][27], int8_t R_matrix[])
+int unhash(const int* hash, int8_t return_value[])
+{
+	int8_t *indices;
+	indices = (int8_t*)(hash);
+	for(int i = 0;i<3;i++)
+	{
+		int8_t byte;
+		byte = indices[i]/(3*3);
+		byte--;
+		return_value[i*3] = byte;
+
+		byte = (indices[i]/3)%3;
+		byte--;
+		return_value[i*3 + 1] = byte;
+
+		byte = indices[i]%3;
+		byte--;
+		return_value[i*3 + 2] = byte;
+	}
+
+	return 0;
+}
+
+bool setRMatrix(const int8_t current_state[], int8_t R_tensor[][27][27][9], int8_t R_matrix[])
 {
 	int hash = getHash(current_state);
-	if((hash&0xff) == 0xff)
+	if((hash&0xff000000) != 0x00)
 		return false;
-	R_tensor[hash>>24][hash>>16][hash>>8] = R_matrix;
+	memcpy(R_tensor[hash>>24][hash>>16][hash>>8],R_matrix, 9);
 	return true;
 }
